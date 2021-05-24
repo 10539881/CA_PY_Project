4f43dfb5730de8179c8e3a7e35429d398e3089dd
10539881 <10539881@mydbs.ie>
2021-05-24 12:55:47 +0100

    updates

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import pyodbc
 import pandas as pd
 from flask import Flask, render_template, request, redirect, url_for, session, flash
-from flask_sqlalchemy import sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine, Table, MetaData, select, or_, and_, insert
 from sqlalchemy.sql.operators import notendswith_op
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -10,6 +10,7 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = '24681012141618'
     return app
+### three # means these statements were used with sql alchemy but I couldn't get it to work, so once sql server statements are workign you can remove all lines starting with ###
 
 app = create_app()
 
@@ -19,15 +20,22 @@ driver = 'ODBC Driver 17 for SQL Server'
 username = 'sa' 
 
 
-with open(".pw") as f:
-    password = f.read()
+###app.config['SQLALCHEMY_DATABASE_URI'] = f'mssql+pyodbc://{username}:{password}@{server}/{database}?driver={driver}'
 
-#setting up a sql connection
-database_connection = f'mssql+pyodbc://{username}:{password}@{server}/{database}?driver={driver}'
-engine = create_engine(database_connection)
-connection = engine.connect()
+###db = SQLAlchemy(app)
 
-metadata = MetaData()
+connection = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+cursor = connection.cursor() 
+
+#with open(".pw") as f:
+ #   password = f.read()
+
+###setting up a sql connection
+###database_connection = f'mssql+pyodbc://{username}:{password}@{server}/{database}?driver={driver}'
+###engine = create_engine(database_connection)
+###connection = engine.connect()
+
+###metadata = MetaData()
 
 
 @app.route('/', methods=['GET','POST']) #this is the function to login a user
@@ -36,11 +44,16 @@ def login():
         if 'email' in request.form and 'password' in request.form:
             email = request.form['email'] 
             password = request.form['password']
-            user = Table('UserDetails', metadata, autoload=True, autoload_with=engine)
-            stmt = select([user])
-            stmt = stmt.where(and_(user.columns.Email == email, user.columns.Password == password))
-            result_proxy = connection.execute(stmt).fetchone()
-            if result_proxy != []:
+            ###user = Table('UserDetails', metadata, autoload=True, autoload_with=engine)
+            ###stmt = select([user])
+            ###stmt = stmt.where(and_(user.columns.Email == email, user.columns.Password == password))
+            ###result_proxy = connection.execute(stmt).fetchone()
+            SQLCommand = ("SELECT * from UserDetails") 
+            #SQLCommand = ("SELECT * from UserDetails WHERE Email = '" + email + "'") 
+            #Processing Query  
+            cursor.execute(SQLCommand)
+            results = cursor.fetchall()
+            if results != []:
                 session['loginsuccess'] = True
                 return redirect(url_for('profile'))
             else:
@@ -49,6 +62,12 @@ def login():
 
     return render_template("login.html")
 
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+"""
 @app.route('/new_user', methods=['GET','POST']) #this is the function to register a new user
 def new_user():
     if request.method=='POST':
@@ -108,12 +127,11 @@ def logout():
 @app.route('/contact', methods=['GET','POST'])
 def contactus():
     #session.pop('loginsuccess', None)
-    return redirect(url_for('contactus'))
+    return render_template("contact.html")
+"""
 
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port='8080', ssl_context=("../cert.pem","../privkey.pem"))
 
-    
+    #, host='0.0.0.0', port='8080', ssl_context=("../cert.pem","../privkey.pem")
 
 
 
